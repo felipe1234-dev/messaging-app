899afadff859a37e0a345b35ee3b3f9f5f903424
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-19 16:49:39 -0300

    add(client/styles/layout): props to change Icon spacing

--- client/src/styles/layout/Icon.tsx
+++ client/src/styles/layout/Icon.tsx
@@ -4,6 +4,10 @@ import styled from "styled-components";
 interface StyledIconProps {
     variant: Variant;
     size: number;
+    pl: number;
+    pr: number;
+    pb: number;
+    pt: number;
 }
 
 const StyledIcon = styled.span<StyledIconProps>`
@@ -13,6 +17,10 @@ const StyledIcon = styled.span<StyledIconProps>`
     color: ${(props) => props.theme.text[props.variant]};
     width: ${(props) => props.size}em;
     height: auto;
+    padding-top: ${({ pt }) => pt}px;
+    padding-left: ${({ pl }) => pl}px;
+    padding-right: ${({ pr }) => pr}px;
+    padding-bottom: ${({ pb }) => pb}px;
 
     svg {
         display: block;
@@ -26,10 +34,24 @@ interface IconProps extends Partial<StyledIconProps> {
 }
 
 function Icon(props: IconProps) {
-    const { variant = "primary", size = 1 } = props;
+    const { 
+        variant = "primary", 
+        size = 1,
+        pl = 0,
+        pr = 0,
+        pb = 0,
+        pt = 0
+    } = props;
 
     return (
-        <StyledIcon variant={variant} size={size}>
+        <StyledIcon 
+            variant={variant} 
+            size={size}
+            pl={pl}
+            pr={pr}
+            pb={pb}
+            pt={pt}
+        >
             {props.icon}
         </StyledIcon>
     );
