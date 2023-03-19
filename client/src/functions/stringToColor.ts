1895114e3a9035c96b3b52f3af4093721b4cce8c
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-19 16:46:42 -0300

    add(functions): algo to convert text into color

---
+++ client/src/functions/stringToColor.ts
@@ -0,0 +1,21 @@
+function stringToColor(string: string): string {
+    let hash = 0;
+    let i: number;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i++) {
+        hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xff;
+        color += `00${value.toString(16)}`.substr(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+}
+
+export default stringToColor;
