d1b1402fac754af3423af42a207cefb65cecd66c
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-19 15:50:26 -0300

    add(globals/constants): new events - CHAT_UPDATED and MESSAGE_SENT

--- globals/src/constants/events.ts
+++ globals/src/constants/events.ts
@@ -2,6 +2,8 @@ const events = {
     USER_UPDATED: "userUpdated" as const,
     FRIEND_UPDATED: "friendUpdated" as const,
     FRIEND_REQUEST_SENT: "friendRequestSent" as const,
+    CHAT_UPDATED: "chatUpdated" as const,
+    MESSAGE_SENT: "messageSent" as const
 };
 
 export default events;
