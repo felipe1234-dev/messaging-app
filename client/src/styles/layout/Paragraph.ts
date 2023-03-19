c619865c9e67ae12b753b892310db3016b32edc8
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-19 16:50:01 -0300

    add(client/styles/layout): props to change Paragraph spacing

--- client/src/styles/layout/Paragraph.ts
+++ client/src/styles/layout/Paragraph.ts
@@ -4,16 +4,29 @@ import { Variant } from "@types";
 interface ParagraphProps {
     variant?: Variant; 
     size?: number;
+    ml?: number;
+    mr?: number;
+    mt?: number;
+    mb?: number;
 }
 
 const Paragraph = styled.p<ParagraphProps>`
     font-size: ${({ size }) => size}em;
     color: ${({ variant, theme }) => variant && theme.text[variant]};
+
+    margin-left: ${({ ml }) => ml}px;
+    margin-right: ${({ mr }) => mr}px;
+    margin-top: ${({ mt }) => mt}px;
+    margin-bottom: ${({ mb }) => mb}px;
 `;
 
 Paragraph.defaultProps = {
     size: 1,
-    variant: "primary"
+    variant: "primary",
+    ml: 0,
+    mr: 0,
+    mt: 0,
+    mb: 0
 };
 
 export default Paragraph;
