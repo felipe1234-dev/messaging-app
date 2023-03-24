4f66db26c94567e8a074d25b615025165543bfc0
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-23 22:30:33 -0300

    add(client/components): carrousel component

---
+++ client/src/functions/classNames.ts
@@ -0,0 +1,26 @@
+function classNames(...classes: (string | {
+    [className: string]: boolean
+})[]): string {
+   let result = "";
+
+   for (const className of classes) {
+       if (typeof className === "object") {
+           for (const [key, value] of Object.entries(className)) {
+               if (value) {
+                   result += " ";
+                   result += key;
+               }
+           }
+       } else {
+           result += " ";
+           result += className;
+       }
+   }
+
+   result = result.trim();
+   result = result.replace(/\s+/g, " ");
+
+   return result;
+}
+
+export default classNames;
--- client/src/functions/index.ts
+++ client/src/functions/index.ts
@@ -1,3 +1,4 @@
 export { default as isLocal } from "./isLocal";
 export { default as stringToAvatar } from "./stringToAvatar";
 export { default as stringToColor } from "./stringToColor";
+export { default as classNames } from "./classNames";
