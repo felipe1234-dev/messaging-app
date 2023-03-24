9a68ce399f62804481f70925aa723e5f8f2a8001
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-23 22:23:33 -0300

    add(client/components/Avatar): be able to merge multiple images into one

--- client/src/components/Avatar/index.tsx
+++ client/src/components/Avatar/index.tsx
@@ -2,18 +2,36 @@ import { StyledAvatar } from "./styles";
 import { stringToAvatar } from "@functions";
 
 interface AvatarProps {
-    src?: string;
+    src?: string | string[];
     alt?: string;
 }
 
 function Avatar(props: AvatarProps) {
     const { src, alt = "Avatar" } = props;
+    const multiple = src instanceof Array && src.length > 1;
     const colorAvatar = stringToAvatar(alt);
 
-    return (
-        <StyledAvatar backgroundColor={colorAvatar.color}>
-            {src ? <img src={src} alt={alt} /> : colorAvatar.shortName}
+    const Wrapper = ({ children }: { children: React.ReactNode }) => (
+        <StyledAvatar 
+            backgroundColor={colorAvatar.color}
+            multiple={multiple}    
+        >
+            {children}
         </StyledAvatar>
+    );
+
+    if (multiple) {
+        return (
+            <Wrapper>
+                {src.map(src => <img src={src} alt={alt} />)}
+            </Wrapper>
+        );
+    } 
+
+    return (
+        <Wrapper>
+            {src ? <img src={src as string} alt={alt} /> : colorAvatar.shortName}
+        </Wrapper>
     );
 }
 
--- client/src/components/Avatar/styles.ts
+++ client/src/components/Avatar/styles.ts
@@ -2,14 +2,17 @@ import styled from "styled-components";
 
 interface StyledAvatarProps {
     backgroundColor?: string;
+    multiple: boolean;
 }
 
 const StyledAvatar = styled.div<StyledAvatarProps>`
     position: relative;
+
     display: flex;
     align-items: center;
     justify-content: center;
     flex-shrink: 0;
+    ${({ multiple }) => multiple && "gap: 3px;"}
 
     width: 40px;
     height: 40px;
