7f4f85815b3d5c99d0ee811a59117211642d9342
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-25 10:17:34 -0300

    add(globals/constants/events): events for sent messages

--- globals/src/constants/events.ts
+++ globals/src/constants/events.ts
@@ -3,7 +3,10 @@ const events = {
     FRIEND_UPDATED: "friendUpdated" as const,
     FRIEND_REQUEST_SENT: "friendRequestSent" as const,
     CHAT_UPDATED: "chatUpdated" as const,
-    MESSAGE_SENT: "messageSent" as const
+    MESSAGE_SENT: "messageSent" as const,
+    TEXT_MESSAGE_SENT: "textMessageSent" as const,
+    VIDEO_MESSAGE_SENT: "videoMessageSent" as const,
+    AUDIO_MESSAGE_SENT: "audioMessageSent" as const,
 };
 
 export default events;
