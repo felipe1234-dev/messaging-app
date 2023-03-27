64c543607d71ef41b1f9ee6544ae9cef890cd3bd
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-27 01:21:12 -0300

    add(styles/layout/Title): spacing props

--- client/src/styles/layout/Title.tsx
+++ client/src/styles/layout/Title.tsx
@@ -5,20 +5,28 @@ interface StyledTitleProps {
     level: number;
     variant: Variant;
     align: "center" | "start" | "end";
-    ml?: number;
-    mr?: number;
-    mt?: number;
-    mb?: number;
-    pt?: number;
-    pl?: number;
-    pr?: number;
-    pb?: number;
+    ml: number;
+    mr: number;
+    mt: number;
+    mb: number;
+    pt: number;
+    pl: number;
+    pr: number;
+    pb: number;
 }
 
 const StyledTitle = styled.h1<StyledTitleProps>`
     font-size: ${props => 6/props.level}em;
     text-align: ${props => props.align};
     color: ${props => props.theme.text[props.variant]};
+    margin-top: ${({ mt }) => mt}px;
+    margin-left: ${({ ml }) => ml}px;
+    margin-right: ${({ mr }) => mr}px;
+    margin-bottom: ${({ mb }) => mb}px;
+    padding-top: ${({ pt }) => pt}px;
+    padding-left: ${({ pl }) => pl}px;
+    padding-right: ${({ pr }) => pr}px;
+    padding-bottom: ${({ pb }) => pb}px;
 `;
 
 interface TitleProps extends Partial<StyledTitleProps> {
@@ -49,6 +57,10 @@ function Title(props: TitleProps) {
             level={level}
             variant={variant}
             align={align}
+            mt={mt} mb={mb}
+            ml={ml} mr={mr}
+            pt={pt} pb={pb}
+            pl={pl} pr={pr}
         >
             {children}
         </StyledTitle>
