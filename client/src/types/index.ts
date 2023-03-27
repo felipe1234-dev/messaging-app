25a99a45956104329f90273afd0efb7928ffb0c5
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-27 01:24:19 -0300

    add(client/types): interfaces that Chat and User classes that will be useful in the front end context

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,2 +1,16 @@
+import { User, Chat, Message } from "messaging-app-globals";
+
 export type Severity = "error" | "warning" | "info" | "success";
 export type Variant = "primary" | "secondary" | "highlight" | Severity;
+
+export interface WrapperUser extends Omit<User, "friends"> {
+    friends: User[];
+}
+
+export interface WrapperChat extends Omit<Chat, "members" | "blocked" | "admins" | "createdBy" | "deletedBy"> {
+    members: User[];
+    blocked: User[];
+    admins: User[];
+    createdBy: User;
+    messages: Message[];
+}
