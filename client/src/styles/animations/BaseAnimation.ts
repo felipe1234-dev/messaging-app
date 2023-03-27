33ed0e324831192c7d37bdd5b464fdff7bf4aae2
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-27 01:20:35 -0300

    format(client/styles/animations): indent

--- client/src/styles/animations/BaseAnimation.ts
+++ client/src/styles/animations/BaseAnimation.ts
@@ -12,13 +12,13 @@ interface BaseAnimationProps {
 }
 
 const BaseAnimation = styled.div<BaseAnimationProps>`
-  animation-duration: ${props => props.duration};  
-  animation-timing-function: ${props => props.timingFunction};      
-  animation-delay: ${props => props.delay};  
-  animation-iteration-count: ${props => props.iterationCount};  
-  animation-direction: ${props => props.direction}; 
-  animation-fill-mode: ${props => props.fillMode};  
-  animation-play-state:  ${props => props.playState};  
+    animation-duration: ${props => props.duration};  
+    animation-timing-function: ${props => props.timingFunction};      
+    animation-delay: ${props => props.delay};  
+    animation-iteration-count: ${props => props.iterationCount};  
+    animation-direction: ${props => props.direction}; 
+    animation-fill-mode: ${props => props.fillMode};  
+    animation-play-state:  ${props => props.playState};  
 `;
 
 BaseAnimation.defaultProps = {
--- client/src/styles/animations/FadeIn.ts
+++ client/src/styles/animations/FadeIn.ts
@@ -8,12 +8,12 @@ const props = {
 const fadeInAnimation = (
     mode: "from-top" | "from-bottom" = "from-bottom"
 ) => keyframes`
-  from { ${props[mode]}: 0; opacity: 0; }
-  to { ${props[mode]}: 30px; opacity: 1; }
+    from { ${props[mode]}: 0; opacity: 0; }
+    to { ${props[mode]}: 30px; opacity: 1; }
 `;
 
 const FadeIn = styled.div`
-  animation: ${fadeInAnimation()} 0.5s ease-in-out;
+    animation: ${fadeInAnimation()} 0.5s ease-in-out;
 `;
 
 export default FadeIn;
