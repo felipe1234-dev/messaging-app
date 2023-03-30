7d89db9bd82099abd1dfa3d6fc35a80da7d258ab
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-29 23:10:15 -0300

    add(client/constants): export appName

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,3 +1,4 @@
+export const appName = "Chatify";
 export { default as darkPalette } from "./darkPalette";
 export { default as lightPalette } from "./lightPalette";
 export { default as routes } from "./routes";
