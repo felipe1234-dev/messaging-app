d059be34a8f1135ee5ff53209852ad3e918f9a81
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-29 23:14:12 -0300

    add(layout/Columns): spacing props

--- client/src/styles/layout/Columns.ts
+++ client/src/styles/layout/Columns.ts
@@ -5,6 +5,14 @@ interface ColumnsProps {
     justify?: "center" | "space-between" | "space-around" | "start" | "end";
     multiline?: boolean;
     gap?: number;
+    ml?: number;
+    mr?: number;
+    mt?: number;
+    mb?: number;
+    pt?: number;
+    pl?: number;
+    pr?: number;
+    pb?: number;
 }
 
 const Columns = styled.div<ColumnsProps>`
@@ -15,6 +23,16 @@ const Columns = styled.div<ColumnsProps>`
     justify-content: ${({ justify }) => justify};
     gap: ${({ gap }) => gap}px;
     ${({ multiline }) => multiline && "flex-wrap: wrap;"}
+
+    margin-left: ${({ ml }) => ml}px;
+    margin-right: ${({ mr }) => mr}px;
+    margin-top: ${({ mt }) => mt}px;
+    margin-bottom: ${({ mb }) => mb}px;
+
+    padding-top: ${({ pt }) => pt}px;
+    padding-left: ${({ pl }) => pl}px;
+    padding-right: ${({ pr }) => pr}px;
+    padding-bottom: ${({ pb }) => pb}px;
 `;
 
 Columns.defaultProps = {
