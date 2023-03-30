866679c3548c07f0291693868d0d713b82f5279c
Felipe Alves <felipe.alves@taugor.com.br>
2023-03-29 23:12:49 -0300

    add(Home): new layout

---
+++ client/src/pages/Home/Page/LeftColumn/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "@styles/layout";
+
+function LeftColumn() {
+    return (
+        <Container
+            variant="secondary"
+            justify="start"
+            align="center"
+            direction="column"
+            borderRadius="10px"
+        >
+
+        </Container>
+    );
+}
+
+export default LeftColumn;
--- client/src/pages/Home/Page/index.tsx
+++ client/src/pages/Home/Page/index.tsx
@@ -1,23 +1,21 @@
 import { Columns, Column } from "@styles/layout";
 import { useAuth } from "@providers";
-import Nav from "./Nav";
+
 
 function Page() {
     const { user } = useAuth();
 
     if (!user) return <></>;
 
-    return (<>
-        <Nav />
+    return (
         <Columns>
             <Column>
-                {user.name}
+                
             </Column>
             <Column>
-                test
             </Column>
         </Columns>
-    </>);
+    );
 }
 
 export default Page;
