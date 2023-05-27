ecda4f17dd036bc92c520b8b7aa68928e807ec2f
Felipe Alves <felipe.alves@taugor.com.br>
2023-05-27 16:45:33 -0300

    fix(providers/Loader): loader not visible when user's session is still being fetched

--- client/src/providers/Loader.tsx
+++ client/src/providers/Loader.tsx
@@ -10,11 +10,13 @@ interface LoaderValue {
 const LoaderContext = createContext<LoaderValue | undefined>(undefined);
 
 function LoaderProvider(props: { children: React.ReactNode }) {
-    const [visible, setVisible] = useState(false);
+    const [processes, setProcesses] = useState(0);
+    const visible = processes > 0;
     const delay = 3000;
 
-    const show = () => setVisible(true);
-    const hide = () => setTimeout(() => setVisible(false), delay);
+    const show = () => setProcesses((prev) => prev + 1);
+    const hide = () =>
+        setTimeout(() => setProcesses((prev) => Math.max(prev - 1, 0)), delay);
 
     return (
         <LoaderContext.Provider
