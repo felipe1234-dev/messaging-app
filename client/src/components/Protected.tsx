df4eeccca5fcd2bd7fee6683eb30c7f66862347b
Felipe Alves <felipe.alves@taugor.com.br>
2023-05-27 16:41:56 -0300

    refactor(components/Protected): simplify logic

--- client/src/components/Protected.tsx
+++ client/src/components/Protected.tsx
@@ -1,15 +1,9 @@
-import { useState, useEffect } from "react";
 import { useAuth } from "@providers";
 import { Login } from "@pages";
 
 function Protected(props: { children: React.ReactNode }) {
     const { user } = useAuth();
-    const [allowed, setAllowed] = useState(false);
-
-    useEffect(() => {
-        if (!user) setAllowed(false);
-        else setAllowed(true);
-    }, [user]);
+    const allowed = !!user;
 
     if (!allowed) return <Login />;
 
